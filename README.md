e96b09eb416c9f3bb2a1291788b1536df04f4649
Greg Lawson <glawson6@yahoo.com>
2015-02-04 14:05:16 -0500

    Added support for Microsoft Word

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+soffice --convert-to pdf filename.doc  --headless
+
+
+
+
+--convert-to doc:"MS Word 97"            # produces a dot graphic
+--convert-to doc:"MS Word 2003 XML"      # measure_conversion.xsl: Find no conversion for  to 'twip'!
+--convert-to doc:"MS Word 2007 XML"      # produces a graphic with a "read-error"
+--convert-to docx:"Office Open XML Text" # produces a graphic with a "read-error"
--- app.rb
+++ app.rb
@@ -22,7 +22,44 @@ get '/pdf' do
   kit.stylesheets << CSS_DIR + '/bootstrap.min.css'
   kit.stylesheets << CSS_DIR + '/main.css'
   #send_file 'foo.png'
+  puts kit.command
   stream do |out|
     out << kit.to_pdf
   end
+end
+
+
+get '/doc' do
+  content_type 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+  html = erb(:resume_pdf,:layout => false)
+  stream do |out|
+    out << to_doc(html)
+  end
+end
+
+def to_doc(html)
+  file_name = "tem.txt.html"
+  file = File.new(file_name,"wb+")
+  file.puts(html)
+  commands = ["/opt/homebrew-cask/Caskroom/libreoffice/4.4.0/LibreOffice.app/Contents/MacOS/soffice",'--convert-to docx:"Office Open XML Text"',"doc"]
+  commands << file_name
+  commands << "--headless"
+  invoke = commands.join(" ")
+  puts invoke
+  result = IO.popen(invoke, "wb+") do |doc|
+    #doc.puts(@source.to_s) if @source.html?
+    doc.close_write
+    doc.gets(nil)
+  end
+
+  #result = File.read(path) if path
+
+  # $? is thread safe per http://stackoverflow.com/questions/2164887/thread-safe-external-process-in-ruby-plus-checking-exitstatus
+  #raise "command failed (exitstatus=#{$?.exitstatus}): #{invoke}" if result.to_s.strip.empty? or !successful?($?)
+  return File.binread("tem.txt.docx")
+end
+
+def to_file(path)
+  self.to_pdf(path)
+  File.new(path)
 end
